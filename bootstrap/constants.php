f37500e0d68b1c26253bfbf48aea3f008c41f93c
BLACK CODE <111893008+teamblackcode@users.noreply.github.com>
2024-03-01 22:18:42 +0330

    Delete bootstrap directory

--- bootstrap/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-# FileUplaoder service constants
-define('BASE_PATH', 'C:/xampp/htdocs/blackcode-weblog/');
-define('BASE_URL', 'http://localhost/blackcode-weblog/');
-define('IMAGES_FILE', BASE_PATH.'assets/images/');
-define('MAX_FILE_SIZE', 3097152);
-define('ALLOWED_SUFFIX', ['image/jpeg', 'image/png']);
