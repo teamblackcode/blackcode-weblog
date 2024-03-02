458bb3ec0e83588196b9c83a03d43bd5567d8fe6
mehdishahi <mahdishahipro@gmail.com>
2024-03-02 11:53:43 +0330

    create api caching service and response class for api requests

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -2,6 +2,10 @@
 # FileUplaoder service constants
 define('BASE_PATH', 'C:/xampp/htdocs/blackcode-weblog/');
 define('BASE_URL', 'http://localhost/blackcode-weblog/');
-define('IMAGES_FILE', BASE_PATH.'assets/images/');
+define('IMAGES_FILE', BASE_PATH . 'assets/images/');
 define('MAX_FILE_SIZE', 3097152);
 define('ALLOWED_SUFFIX', ['image/jpeg', 'image/png']);
+
+# cache service constants
+define('CACHE_ENABLED', 1);
+define('CACHE_DIR', __DIR__ . '/cache/');
