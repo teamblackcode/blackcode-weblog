64bfaade42e98411b38b999b9fafca6f7199e42e
Mahdi Shahi <mahdishahipro@gmail.com>
2024-03-28 13:48:04 +0330

    develop config helper (get file contents)

---
+++ Backend/App/Helpers/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Exceptions\ConfigFileNotFoundException;
+
+class Config
+{
+    public static function getFileContents(string $fileName)
+    {
+        $filePath = realpath(__DIR__ . '/../configs/' . "$fileName" . '.php');
+        if (!$filePath) {
+            throw new ConfigFileNotFoundException();
+        }
+        $fileContents = require $fileName;
+        return $fileContents;
+    }
+    public static function get(string $fileName, string $key = null)
+    {
+        $fileContents = self::getFileContents($fileName);
+        if (is_null($key)) return $fileContents;
+        return $fileContents[$key] ?? null;
+    }
+}
